fcbad9a19a42688ded49c6beddd48d751026c31e
Wlad199 <habilis199@mail.ru>
2023-06-23 19:09:25 +0300

    popular

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,6 +5,21 @@ import { flsModules } from "./modules.js";
 
 
 // Функционал слайдера - слик(MW 3.2)
-//$(document).ready(function () {
-//	$('.popular__content').slick();
-//})
+$(document).ready(function () {
+	$('.popular__content').slick({
+		arrows: true, // стрелки
+		dots: false, // точки
+		//adaptiveHeight: true, // адаптивная высота (false)
+		slidesToShow: 3, // кол-во показ. слайдов
+		touchThreshold: 10, // расстояние для считывания свайпа (5 1/5 экрана)
+		centerMode: true, // добавляет класс к центральному слайду (false)
+		//autoplay: true, // автовоспроизведение (false)
+		//autoplaySpeed: 100, // скорость (3000)
+		responsive: [
+			{
+				breakpoint: 550,
+				settings: "unslick",
+			}
+		]
+	});
+})
